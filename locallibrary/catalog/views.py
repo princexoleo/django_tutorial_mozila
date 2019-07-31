7e3ae9a99fdd3cf5eb12b5042ce2351da326068a
princexoleo <xtleon.xl@gmail.com>
2019-07-31 23:25:58 +0600

    add views to url

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from catalog.models import Genre, Author ,Book, BookInstance
 # Create your views here.
+def index(request):
+    """ View Function for Home page of the site """
+    # Generates count of some of the main objects
+    num_books       = Book.objects.all().count()
+    num_instances   = BookInstance.objects.all().count()
+
+    # Available Books ('status='a')
+    num_instances_available     = BookInstance.objects.filter(status__exact='a').count()
+    #number of author
+    num_author      =Author.objects.count()
+
+    return render()
